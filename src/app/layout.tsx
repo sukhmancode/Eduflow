a87096cc587cb59ae700b41f9b0b1ea26ba2d254
sukhmancode <coder4614@gmail.com>
2025-04-07 10:22:04 +0530

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning className="dark">
+    <html lang="en" suppressHydrationWarning >
       <body className={`${inter.className} `} >
      
           {children}
